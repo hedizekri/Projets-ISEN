36e4a39371e07ebb36451bb43c0616d6e363bfe3
clementvillalba <43475249+clementvillalba@users.noreply.github.com>
2018-10-05 15:06:01 +0200

    tous les produit connection db

--- pages/tous_les_produits.php
+++ pages/tous_les_produits.php
@@ -81,6 +81,35 @@ require('outils.php') ;
     <div id="contenu">
     <h1>Tous les produits</h1>
     <hr />
+    
+    <?php
+
+    // appel de la fonction de connexion
+    $connexion = mysqli_connect("localhost","root","", "sap2lux");
+
+      //on test la connexion
+      if (!$connexion){
+        die("Connexion impossible : ".mysqli_connect_error($connexion));
+      }
+
+
+      // Modification du jeu de résultats en utf8
+      mysqli_set_charset($connexion, 'utf8');
+
+      // Création de la requête
+      $codeSQL ="SELECT IDvetement,titre,photo,prix,texte from vetement" ;
+
+      // lancement effectif de la requête SQL 
+      $resultat = mysqli_query($connexion,$codeSQL);
+      
+        // si jamais la requête se passait mal      
+        if ( ! $resultat ) 
+          die ("Requete SQL invalide : " . mysqli_error($connexion));
+        
+        // on teste le nombre de réponse de la requet
+        $nombreLigne = mysqli_num_rows($resultat) ;
+
+    ?>
     </div> <!-- fin contenu -->
 
 
