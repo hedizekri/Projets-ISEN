fc41d1cbabba4ece7b336ecc6f3268ea4ae3ded2
hedizekri <43677230+hedizekri@users.noreply.github.com>
2018-10-05 14:24:54 +0200

    Revert "Site de cuisine"
    This reverts commit 8b080346938fb0d4dde290c2bf9f05779f70927b.

--- pages/outils.php
+++ pages/outils.php
@@ -116,7 +116,7 @@ echo '
 
                 <ul>
 
-                  <li><a href="Infosap2lux.html">Information sur la marque</a></li>
+                  <li><a href="informations_marque.html">Information sur la marque</a></li>
 
           </tr>
 
