84150da3a0ced397baf321e7142b59d7a48ca6dd
clementvillalba <43475249+clementvillalba@users.noreply.github.com>
2018-11-07 13:57:55 +0100

    Update pages/connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -75,27 +75,81 @@ require('outils.php') ;
 
     <!-- ******************************************************* -->
 
+<?php
+/*
+Page: connexion.php
+*/
+session_start(); // à mettre tout en haut du fichier .php, cette fonction propre à PHP servira à maintenir la $_SESSION
+if(isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
+    // on vérifie que le champ "identifiant" n'est pas vide
+    // empty vérifie à la fois si le champ est vide et si le champ existe belle et bien (is set)
+    if(empty($_POST['identifiant'])) {
+        echo "Le champ identifiant est vide.";
+    } else {
+        // on vérifie maintenant si le champ "Mot de passe" n'est pas vide"
+        if(empty($_POST['mdp'])) {
+            echo "Le champ Mot de passe est vide.";
+        } else {
+            // les champs sont bien posté et pas vide, on sécurise les données entrées par le membre:
+            $identifiant = htmlentities($_POST['identifiant'], ENT_QUOTES, "ISO-8859-1"); // le htmlentities() passera les guillemets en entités HTML, ce qui empêchera les injections SQL
+            $MotDePasse = htmlentities($_POST['mdp'], ENT_QUOTES, "ISO-8859-1");
+            //on se connecte à la base de données:
+            $mysqli = mysqli_connect("localhost", "root", "", "sap2lux");
+            //on vérifie que la connexion s'effectue correctement:
+            if(!$mysqli){
+                echo "Erreur de connexion à la base de données.";
+            } else {
+                // on fait maintenant la requête dans la base de données pour rechercher si ces données existe et correspondent:
+                $Requete = mysqli_query($mysqli,"SELECT * FROM membres WHERE identifiant = '".$identifiant."' AND  mdp = '".md5($MotDePasse)."'");//si vous avez enregistré le mot de passe en md5() il vous suffira de faire la vérification en mettant mdp = '".md5($MotDePasse)."' au lieu de mdp = '".$MotDePasse."'
+                // si il y a un résultat, mysqli_num_rows() nous donnera alors 1
+                // si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
+
+                if(mysqli_num_rows($Requete) == 0) {
+                    echo "Le identifiant ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
+                } else {
+                    // on ouvre la session avec $_SESSION:
+                    $_SESSION['identifiant'] = $identifiant; // la session peut être appelée différemment et son contenu aussi peut être autre chose que le identifiant
+                    echo "Vous êtes à présent connecté !";
+                }
+            }
+        }
+    }
+}
+?>
+
 <div id='connexion_inscription'>  
 
-      <form method="Post" action="authentification.php">
+      <!-- 
+Les balises <form> sert à dire que c'est un formulaire
+on lui demande de faire fonctionner la page connexion.php une fois le bouton "Connexion" cliqué
+on lui dit également que c'est un formulaire de type "POST"
+ 
+Les balises <input> sont les champs de formulaire
+type="text" sera du texte
+type="password" sera des petits points noir (texte caché)
+type="submit" sera un bouton pour valider le formulaire
+name="nom de l'input" sert à le reconnaitre une fois le bouton submit cliqué, pour le code PHP
+ -->
+
+      <form method="Post" action="connexion.php">
 
   
         <h2> Connexion </h2>
         <label>Identifiant</label>
 
-        <input type="text" name="identifiant" size="30" maxlength="30"/>
+        <input type="text" name="identifiant" value="" />
 
         <br/>
         <br/>
 
         <label>Mot de passe</label>
 
-        <input type="password" name="motdepasse" size="15" maxlength="15"/>
+        <input type="password" name="mdp" value="" />
 
         <br/>
         <br/>
 
-        <input type="submit" name="valider" value="Valider">
+        <input type="submit" name="connexion" value="Connexion" />
 
         <br/>
 
