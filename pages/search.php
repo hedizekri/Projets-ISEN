59cb4abc635ce76e7ca88bb47296432f9b261b81
clementvillalba <43475249+clementvillalba@users.noreply.github.com>
2018-11-09 10:40:04 +0100

    search

---
+++ pages/search.php
@@ -0,0 +1,125 @@
+<?php
+
+session_start();
+require('outils.php') ;
+
+?>
+
+<!DOCTYPE html>     <!-- PAGE : MODELE.HTML -->
+
+
+
+<html>
+
+
+
+<head>
+
+  <title>Sap2lux</title>
+
+  <meta charset="utf-8" />
+
+
+
+
+    <link href="../styles/styles.css"  rel="stylesheet" type="text/css" />
+    <script language="javascript" src="fonction.js"></script>
+
+
+
+
+
+
+
+
+</head>
+
+
+
+
+
+<body>
+
+            
+
+    <!-- ******************************************************* -->
+
+    <!--                 MENU                                    -->
+
+    <!-- ******************************************************* -->
+
+    
+
+    <div id="titre">
+    
+      <?php
+
+        afficheTitre();
+
+      ?>
+
+    <div id="menu">
+    
+           <?php
+
+        afficheMenu();
+
+      ?>   
+    
+    </div> <!-- fin menu -->
+
+    
+
+    <!-- ******************************************************* -->
+
+    <!--                 CONTENU                                 -->
+
+    <!-- ******************************************************* -->
+
+    
+
+    <div id="contenu">
+
+    
+
+
+    </div> <!-- fin contenu -->
+
+
+
+         
+
+    <!-- ******************************************************* -->
+
+    <!--                 PIED DE PAGE                            -->
+
+    <!-- ******************************************************* -->
+
+    
+
+    <footer>
+
+       <?php
+        afficheFooter();
+    ?>  
+
+    </footer> <!-- fin du pied de page -->
+
+
+
+
+
+</body>
+
+
+
+</html> 
+
+
+
+
+
+
+
+
+
